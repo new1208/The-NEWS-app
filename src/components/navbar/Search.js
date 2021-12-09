6e6c42ab4af7a976671e8e8c538a3bef365ab85f
Rita Shih _ FJU <55233170+rItashih-lol@users.noreply.github.com>
2021-12-09 16:18:19 +0800

    first commit

---
+++ src/components/navbar/Search.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import "./search.css"
+
+const Search = () => {
+    return (
+        <div>
+            <div className="navbar">
+            <h3>Notifications</h3>
+            <h3>About</h3>
+            <h3>Dashboard</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Search
